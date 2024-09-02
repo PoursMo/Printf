6e4c87a68a74da517d95b9ed8fd892fcae413cdc
amaury <amaury@debian.home>
2024-09-02 18:56:57 +0200

    test

--- tester.c
+++ tester.c
@@ -5,6 +5,6 @@
 int main()
 {
 	int i = 9516284;
-	printf("%d\n", printf("%s ceci est %d en hex :%x\net son adresse :%p\n, %d", "coucou", i, i, &i, 654321));
-	ft_printf("%d\n", ft_printf("%s ceci est %d en hex :%x\net son adresse :%p\n, %d", "coucou", i, i, &i, 654321));
+	printf("\n%d caracteres\n", printf("%s ceci est %d en hex :%x\net son adresse :%p\n%d", "coucou", i, i, &i, 654321));
+	ft_printf("\n%d caracteres\n", ft_printf("%s ceci est %d en hex :%x\net son adresse :%p\n%d", "coucou", i, i, &i, 654321));
 }
